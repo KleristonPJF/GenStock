f7e551c15231ea05c0ccbef66995dc6467b630c7
thiagao <thiagobsouza05@gmail.com>
2025-02-13 22:33:53 -0500

    tabela venda

--- sql/genstock.sql
+++ sql/genstock.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12/02/2025 às 07:00
+-- Tempo de geração: 14/02/2025 às 04:32
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -41,7 +41,8 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`idcliente`, `cliente`, `cpfcnpj`, `telefone`, `endereco`, `obs`) VALUES
-(1, 'Kleriston Kaua Pereira Jacome Ferreira', '999.999.999-99', '+55 9 9999-9999', 'Rua acula, n102', 'Ele é um macaco');
+(1, 'Kleriston Kaua Pereira Jacome Ferreira', '999.999.999-99', '+55 9 9999-9999', 'Rua acula, n102', 'Ele é um macaco'),
+(2, 'Thiago Brito de Souza', '000.000.000-00', '+00 0 0000-0000', 'rua ali, n000', 'mano brown');
 
 -- --------------------------------------------------------
 
@@ -83,7 +84,8 @@ CREATE TABLE `produto` (
 --
 
 INSERT INTO `produto` (`idproduto`, `produto`, `tipo`, `quilos`) VALUES
-(1, 'Batata', 'Saco', '24');
+(1, 'Batata', 'Saco', '24'),
+(2, 'Tomate', 'Caixa', '10');
 
 -- --------------------------------------------------------
 
@@ -103,7 +105,22 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`idusuario`, `usuario`, `senha`) VALUES
 (1, '123', '$2y$10$Qi7f1brNHyP5nPGpIX/vPeHv.nWya5KEj.TXTELoyw0OwRQvSPDCm'),
-(2, 'Kleriston', '$2y$10$YfDD9P/1hwX0gQzX4FdCFeYdPliHO5rFCRe6OBRbK0JO4J4LWN2Y.');
+(2, 'Kleriston', '$2y$10$YfDD9P/1hwX0gQzX4FdCFeYdPliHO5rFCRe6OBRbK0JO4J4LWN2Y.'),
+(3, 'Thigas', '$2y$10$ddl7EBMhYsW3uTadxB/W/eZ8yazb1ZpdELMRnWY6ZFBlypY.LK.LK');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `vendas`
+--
+
+CREATE TABLE `vendas` (
+  `idvenda` int(11) NOT NULL,
+  `idcliente` int(11) NOT NULL,
+  `idproduto` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `datavenda` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tabelas despejadas
@@ -135,6 +152,14 @@ ALTER TABLE `usuario`
   ADD PRIMARY KEY (`idusuario`);
 
 --
+-- Índices de tabela `vendas`
+--
+ALTER TABLE `vendas`
+  ADD PRIMARY KEY (`idvenda`),
+  ADD KEY `idcliente` (`idcliente`),
+  ADD KEY `idproduto` (`idproduto`);
+
+--
 -- AUTO_INCREMENT para tabelas despejadas
 --
 
@@ -142,7 +167,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `estoque`
@@ -154,13 +179,19 @@ ALTER TABLE `estoque`
 -- AUTO_INCREMENT de tabela `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `idproduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idproduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `vendas`
+--
+ALTER TABLE `vendas`
+  MODIFY `idvenda` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restrições para tabelas despejadas
@@ -171,6 +202,13 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `estoque`
   ADD CONSTRAINT `produto_dados` FOREIGN KEY (`produto`) REFERENCES `produto` (`idproduto`);
+
+--
+-- Restrições para tabelas `vendas`
+--
+ALTER TABLE `vendas`
+  ADD CONSTRAINT `vendas_ibfk_1` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`idcliente`),
+  ADD CONSTRAINT `vendas_ibfk_2` FOREIGN KEY (`idproduto`) REFERENCES `produto` (`idproduto`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
