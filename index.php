015b7b5958d7de008b95e29395a5c512004e1b8d
thiagao <thiagobsouza05@gmail.com>
2025-02-17 13:27:37 -0500

    eu sou gay

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 <html lang="pt-br">
 <?php
     define('ACCESS_ALLOWED', true);
-    require_once './html/head.php';
 ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GenStock</title>
+    <link rel="stylesheet" href="./visualizar/css/styles.css">
+</head>
 <body>
     <div class="portal-card">
         <a href="./rotas/rotas.php?acao=navlogin">Login</a>
